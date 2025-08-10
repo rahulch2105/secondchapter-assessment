3a0b570eab30e96b720a01d588685fe23786ab5a
RAHULCH <rahulcch794@gmail.com>
2025-08-10 12:02:39 +0530

    Add missing UserList component

---
+++ frontend/src/UserList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserList: React.FC = () => {
+  return (
+    <div>
+      <h2>User List</h2>
+      {/* TODO: Add user list rendering here */}
+    </div>
+  );
+};
+
+export default UserList;
