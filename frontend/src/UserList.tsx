b8e306c026c8e4d3f6c8a8b98d1e7264cd1aff67
RAHULCH <rahulcch794@gmail.com>
2025-08-10 12:09:49 +0530

    Add UserForm and UserList components

--- frontend/src/UserList.tsx
+++ frontend/src/UserList.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserList from './UserList';
 
-const UserList: React.FC = () => {
+const UserForm: React.FC = () => {
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    alert(`User added: ${name}`);
+    setName('');
+  };
+
   return (
     <div>
-      <h2>User List</h2>
-      {/* TODO: Add user list rendering here */}
+      <h2>Add User</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter user name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <UserList />
     </div>
   );
 };
 
-export default UserList;
+export default UserForm;
